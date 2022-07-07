6285546903b41f1fc01a2bb7fda1fbe5dc5c91b9
gabrielhnunez39166 <gabrielhnunez@outlook.com.ar>
2022-07-07 09:51:24 -0300

    Subo la posible solucion del TP 3

--- 8-TPs/03-jsFerretePinturas.js
+++ 8-TPs/03-jsFerretePinturas.js
@@ -2,12 +2,23 @@
 A.	Al ingresar una temperatura en Fahrenheit debemos mostrar la temperatura en Centígrados con un mensaje concatenado (ej.: " 32 Fahrenheit son 0 centígrados").
 B.	Al ingresar una temperatura en Centígrados debemos mostrar la temperatura en Fahrenheit (ej.: "0  centígrados son 32 Fahrenheit ").
 */
+
 function FahrenheitCentigrados () 
 {
-	
+    let temperaturaIngresada = document.getElementById("txtIdTemperatura").value;
+    temperaturaIngresada = parseInt(temperaturaIngresada);
+    let fahrenheit = 32;
+    let fahrenheitCentigrados = temperaturaIngresada - fahrenheit;
+    let mensaje = "La temperatura en Fahrenheit es: " + fahrenheitCentigrados;
+    alert(mensaje);
 }
 
 function CentigradosFahrenheit () 
 {
-	
+    let temperaturaIngresada = document.getElementById("txtIdTemperatura").value;
+    temperaturaIngresada = parseInt(temperaturaIngresada);
+    let fahrenheit = 32;
+    let centigradosFahrenheit = temperaturaIngresada + fahrenheit;
+    let mensaje = "La temperatura en Fahrenheit es: " + centigradosFahrenheit;
+    alert(mensaje);
 }
