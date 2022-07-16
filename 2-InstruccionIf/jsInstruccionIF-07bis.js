18de7f7461dd482f1224022f722f6cea581f6de3
gabrielhnunez39166 <gabrielhnunez@outlook.com.ar>
2022-07-16 11:56:15 -0300

    Ejercicio jsInstruccionI-07bis

---
+++ 2-InstruccionIf/jsInstruccionIF-07bis.js
@@ -0,0 +1,64 @@
+/*
+Nuñez Hugo Gabriel
+Ejercicio 07 BIS V1
+*/
+
+function mostrar() {
+	//Declaro las variables.
+	let edadIngresada;
+	let mensaje;
+
+	//Asigno valores a las variables
+	edadIngresada = document.getElementById("txtIdEdad").value;
+	edadIngresada = parseInt(edadIngresada);
+	
+	if (edadIngresada < 13)
+	{
+		mensaje = "Feliz día";
+	} 
+	else 
+	{
+		if (edadIngresada > 12 && edadIngresada <18) 
+		{
+			mensaje = "Usted es adolescente";
+
+			if (edadIngresada == 17)
+			{
+				mensaje =  mensaje + " último año!!!";
+			}
+		}
+		else
+		{
+			if (edadIngresada > 17)
+			{
+				mensaje = "Tenes edad de laburar";
+
+				if (edadIngresada == 33)
+				{
+					mensaje = mensaje + " como cristo";
+				}
+				else 
+				{
+					if (edadIngresada > 60)
+					{
+						mensaje = "A jubilarse";
+					}
+					else 
+					{
+						if (edadIngresada == 88)
+						{
+							mensaje = "Lindo número";
+						}
+					}
+				}
+			}
+		}
+
+		if (edadIngresada%2 == 0)
+		{
+			mensaje = mensaje + " sos par";
+		}
+	}
+
+	alert(mensaje);
+} //Fin de la funcion principal.
